80afa9a72cd21ea1cfe5b8542bf6816a1c39024c
saba-ramezani <saba.rad.sr@gmail.com>
2024-09-11 14:01:06 +0330

    added photo editing part of services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -9,7 +9,7 @@ const Services = () => {
         <h1 className='h2'>Generative AI made for creators</h1>
         <p className='body-2 mt-4 text-n-4'>Brainwave unlocks the potential of AI-powered applications</p>
       </div>
-      <div className='flex border-[1px] border-gray-800 bg-circulat-pink-dark-gradient w-[100%] relative rounded-[20px] mb-10 h-[600px]'>
+      <div className='flex border-[1px] border-gray-800 bg-circulat-pink-dark-gradient w-[90%] relative rounded-[20px] mb-10 h-[600px]'>
         <div className='flex sm:flex-row '>
           <div className='absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto'>
             <img
@@ -42,13 +42,24 @@ const Services = () => {
           <img src={loading} alt="" className="w-5 h-5 mr-4"/>
           AI is generating</div>
       </div>
-      <div className='flex flex-col lg:flex-row hidden'>
-        <div className='flex flex-col'>
-          <img src={service2} alt="" />
-          <h1>Photo editing</h1>
-          <p>Automatically enhance your photos using our AI app's photo editing feature. Try it now!</p>
+      <div className='grid gap-5 lg:grid-cols-2 w-[90%]'>
+        <div className='flex flex-col border-[1px] border-gray-800 bg-circulat-pink-dark-gradient w-[100%] rounded-[20px] inset-0 relative h-[700px]'>
+          <img
+            src={service2}
+            className="h-full w-full object-cover rounded-[20px]"
+            width={630}
+            height={700}
+            alt="robot"
+          />
+          <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
+            <h4 className="h4 mb-4">Photo editing</h4>
+            <p className="body-2 mb-[3rem] text-n-3">
+              Automatically enhance your photos using our AI app&apos;s
+              photo editing feature. Try it now!
+            </p>
+          </div>
         </div>
-        <div className='flex flex-col'>
+        <div className='flex flex-col border-[1px] border-gray-800 bg-circulat-pink-dark-gradient w-[100%] relative rounded-[20px]'>
           <h1>Video generation</h1>
           <p>The world’s most powerful AI photo and video art generation engine. What will you create?</p>
           <div></div>
